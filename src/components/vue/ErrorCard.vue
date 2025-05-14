b8f3adbfd89936621ec81ee24a28038579d4d1ab
Skid Vis <skid@skidvis.com>
2025-05-14 18:33:58 -0500

    init

---
+++ src/components/vue/ErrorCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="error-card" v-for="error in errors"></div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            errors: [],
+        };
+    },
+};
+</script>
+<style>
+.error-card {
+    background-color: #eee;
+}
+</style>
